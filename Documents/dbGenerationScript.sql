48869fa160e85c9fa5c191f9f3b3324db550b547
Oleksandr Kuzmeniuk <oleksandrkuzmenuk2@gmail.com>
2024-10-27 20:20:06 +0200

    Changed types of columns in sql script

--- Documents/dbGenerationScript.sql
+++ Documents/dbGenerationScript.sql
@@ -3,20 +3,20 @@ GO
 
 CREATE TABLE [InventoryManagement].[dbo].[Categories] (
     [Id] INT IDENTITY (1, 1) PRIMARY KEY,
-    [Name] VARCHAR(255) NOT NULL,
+    [Name] NVARCHAR(255) NOT NULL,
 );
 
 CREATE TABLE [InventoryManagement].[dbo].[Suppliers] (
     [Id] INT IDENTITY (1, 1) PRIMARY KEY,
-    [Name] VARCHAR(255) NOT NULL,
+    [Name] NVARCHAR(255) NOT NULL,
 );
 
 CREATE TABLE [InventoryManagement].[dbo].[Products] (
     [Id] INT IDENTITY (1, 1) PRIMARY KEY,
-    [Title] VARCHAR(255) NOT NULL,
+    [Title] NVARCHAR(255) NOT NULL,
     [Amount] INT NOT NULL CHECK([Amount] >= 0),
     [Price] DECIMAL(19, 4) NOT NULL CHECK([Price] > 0),
-    [Description] TEXT NULL,
+    [Description] NVARCHAR(MAX) NULL,
     [CategoryId] INT FOREIGN KEY REFERENCES [Categories]([Id]),
     [SupplierId] INT FOREIGN KEY REFERENCES [Suppliers]([Id]),
     [LastUpdated] DATETIME NOT NULL
